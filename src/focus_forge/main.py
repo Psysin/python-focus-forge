35231b5f6e0eb18f3064ff6a19d966c836172da4
Psysin <p.proggen@pm.me>
2026-04-13 14:25:16 +0200

    Stand Issue 5, Bug vorjanden, Start Button ändert nicht zu Pause

--- src/focus_forge/main.py
+++ src/focus_forge/main.py
@@ -3,7 +3,11 @@
 This module contains the Flet UI code.
 """
 
+import asyncio
+
 import flet as ft
+
+from focus_forge.timer import format_time
 
 
 def main(page: ft.Page):
@@ -13,6 +17,36 @@ def main(page: ft.Page):
     page.window.height = 450
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    # State Variabklen (Zustand)
+    time_remaining = 1500  # Startwert 25 Minuten in Sekunden
+    is_running = False  # Timer läuft noch nicht
+
+    # Funktion für den Timer
+    async def timer_loop():
+        nonlocal time_remaining, is_running
+        while is_running and time_remaining > 0:
+            await asyncio.sleep(1)
+            time_remaining -= 1
+            countdown.value = format_time(time_remaining)
+            countdown.update()
+
+    # Funktion für Toggle
+    # Einfache Funktion, das Atribut e in Klammern Bedeutet Event z.B. der Klick
+    def toggle_timer(e):
+        # nonlocal bedeutet, das eine Variable außerhalb der Funktion genutzt wird
+        nonlocal is_running
+        # Umschalten True/False
+        is_running = not is_running
+        if is_running:
+            # Button Text ändern
+            btn_start.text = "⏸ Pause"
+            # Async Timer-Loop Starten
+            page.run_task(timer_loop)
+        else:
+            btn_start.text = "▶ Start"
+            # Änderung auf Bildschirm anzeigen
+        page.update()
 
     # Fügt ein Label ein mit Text und gibt Größe und Farbe an.
     # Wir greifen hier auf Objekte von ft (flet) zu per .
@@ -29,7 +63,10 @@ def main(page: ft.Page):
         weight=ft.FontWeight.BOLD,
     )
     # Fügt die Buttons hinzu mit Beschriftung
-    btn_start = ft.ElevatedButton("▶ Start")
+    btn_start_label = ft.Text("▶ Start")
+    btn_start = ft.ElevatedButton(
+        content=btn_start_label, on_click=toggle_timer
+    )  # On_Click sagt tut hier etwas bei einen Click, starte die Funktion toggle_timer
     btn_reset = ft.ElevatedButton("↺ Reset")
 
     # Fügt ein weiteres Anzeige-Element mit Counter hinzu
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -6,7 +6,6 @@ from focus_forge.timer import format_time, get_next_phase, get_phase_duration
 
 # TODO: Issue #4 - Tests für format_time()
 def test_format_time_full_minutes():
-    #     """Test: 1500 Sekunden = 25:00"""
     assert format_time(1500) == "25:00"
 
 
@@ -28,7 +27,7 @@ def test_format_time_negative():
 
 # TODO: Issue #5 - Tests für get_next_phase()
 def test_next_phase_focus_to_break():
-    """Test: Nach Focus kommt Break"""
+
     assert get_next_phase("focus") == "break"
 
 
